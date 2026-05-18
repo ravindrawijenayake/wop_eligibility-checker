a30471240fe6af31e53bc3cf8a1e58ac9670010c
Ravindra Wijenayake <126754516+ravindrawijenayake@users.noreply.github.com>
2026-05-18 13:10:17 +0530

    fix: add base path to vite config for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/wop_eligibility-checker/',
   server: {
     port: 5175
   }
 })
-
-
